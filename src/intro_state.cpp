2161a6d78a7a6a093795c03b19d9d3a7d6a56320
bcarrin3 <billy.carrington.31@gmail.com>
2017-10-27 19:12:51 -0400

    Moved Chest

--- src/intro_state.cpp
+++ src/intro_state.cpp
@@ -10,6 +10,8 @@ IntroState::IntroState(Engine* engine, ErrorHandler* errorHandler)
 
   map = new Map(engine->renderer, errorHandler, LEVEL_1, TILES_TXT,
     &collisionDetector);
+  map->loadSecondTextures(TILES_ADD);
+  map->loadSecondLayout(LEVEL_1_ADD);
 
   setup();
   load();
