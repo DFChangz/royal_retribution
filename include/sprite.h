9390af54a658db92bd4201506f15adfc0a4dbc2a
Idean Labib <idean@genyes.org>
2017-10-27 21:15:29 -0400

    made collision modifications to account for rounding

--- include/sprite.h
+++ include/sprite.h
@@ -69,6 +69,9 @@ class Sprite : public Image {
     bool chest = false;
     bool hole = false;
     bool blade = false;
+
+    void resolveXCollision(Image*, int);
+    void resolveYCollision(Image*, int);
 };
 
 #endif
