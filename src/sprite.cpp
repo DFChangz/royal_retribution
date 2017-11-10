e9b09c0cbe55c5873ed8a6e429eceefb8ec32124
bcarrin3 <billy.carrington.31@gmail.com>
2017-11-10 18:09:01 -0500

    Moving Blades

--- src/sprite.cpp
+++ src/sprite.cpp
@@ -129,6 +129,13 @@ void Sprite::get_texture_size(SDL_Texture *texture, int *width, int *height) {
 /*if the intersection of 2 collidable sprites have more height overlap, y 
 velocity changes direction if more width, x direction changes*/
 void Sprite::notifyCollision(Image* img, doubleRect* intersection, bool resolved) {
+  if(isBlade()){
+    if(img->isSword()){
+      return;
+    }
+    velocityY *=-1; 
+    velocityX *=-1; 
+  }
   if (!resolved) {
   /*if (abs(velocityY) > abs(velocityX)) {
     resolveYCollision(img, intersection->h);
